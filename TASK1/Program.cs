0b7ce9be1170ff162e64f58dd7e93486600b3edc
Elena <panovaelena1@yandex.ru>
2023-02-05 13:55:44 +0300

    Task1 added

---
+++ TASK1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+//0, 7, 8, -2, -2 -> 2
+
+//1, -7, 567, 89, 223-> 3
+
+System.Console.WriteLine("Введите число через пробел: ");
+int [] arr = Array.ConvertAll(Console.ReadLine().Split(), int. Parse);
+System.Console.WriteLine(FindNumbersMoreThenZiro(arr));
+
+int FindNumbersMoreThenZiro(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(arr[i]>0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+
+
